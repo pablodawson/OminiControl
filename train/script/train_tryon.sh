7f1690c7dd67dc37235a6f27d1f5cf78f986da38
pablo <pablodawson2@gmail.com>
2025-02-23 10:52:31 -0300

    train script and config

---
+++ train/script/train_tryon.sh
@@ -0,0 +1,13 @@
+# Specify the config file path and the GPU devices to use
+# export CUDA_VISIBLE_DEVICES=0,1
+
+# Specify the config file path
+export XFL_CONFIG=./train/config/tryon.yaml
+
+# Specify the WANDB API key
+# export WANDB_API_KEY='YOUR_WANDB_API_KEY'
+
+echo $XFL_CONFIG
+export TOKENIZERS_PARALLELISM=true
+
+CUDA_VISIBLE_DEVICES=5,6 accelerate launch --main_process_port 41353 -m src.train.train
